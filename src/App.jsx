9cc0f784eb2507b59dc6f95a289215eda7572e8c
unknown <sagar.inglee@gmail.com>
2025-10-19 18:52:42 +0530

    Project added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { Categories } from "../assets/mockData";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>SHOP BY CATEGORIES</div>
+            <ul>
+              {Categories.map((category, index) => (
+                <li key={index}>{category}</li>
+              ))}
+            </ul>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
