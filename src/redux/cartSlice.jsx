44035514018c93f5d84c7a53972a8642ecaffa36
unknown <sagar.inglee@gmail.com>
2025-10-20 21:39:06 +0530

    Create Redux Store && Build HeroSection

---
+++ src/redux/cartSlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+});
+
+export default cartSlice.reducer;
