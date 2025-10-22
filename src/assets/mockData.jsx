8151958e22067c9c731dae4d647b3fe743f7531f
unknown <sagar.inglee@gmail.com>
2025-10-22 15:19:12 +0530

    Modified assets path

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,13 +1,13 @@
-import Image1 from "./Images/bag.jpg";
-import Image2 from "./Images/sandle.jpg";
-import Image3 from "./Images/watch.jpg";
-import Image4 from "./Images/cap.jpg";
-import Image5 from "./Images/boot1.jpg";
-import Image6 from "./Images/dove.jpg";
-import Image7 from "./Images/headphone.jpg";
-import Image8 from "./Images/robot.jpg";
-import Image9 from "./Images/wwatch.jpg";
-import Image10 from "./Images/shirt.jpg";
+import Image1 from "./images/bag.jpg";
+import Image2 from "./images/sandle.jpg";
+import Image3 from "./images/watch.jpg";
+import Image4 from "./images/cap.jpg";
+import Image5 from "./images/boot1.jpg";
+import Image6 from "./images/dove.jpg";
+import Image7 from "./images/headphone.jpg";
+import Image8 from "./images/robot.jpg";
+import Image9 from "./images/wwatch.jpg";
+import Image10 from "./images/shirt.jpg";
 
 export const Categories = [
   "Electronics",
